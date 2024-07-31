38c4847bf3e971a17e234bfe26f82ccc091f8dfc
Angelo.Crisci <angelocrisci1998@gmail.com>
2024-07-31 17:09:13 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log('JS OK')
+
+// 
+/* 
+Bonus- (0) Recuperiamo l'elemento d'interesse dal DOM.
+1. Chiediamo Il numero di kilometri all'utente.
+2. Chiediamo l'età del passegero.
+3. Calcoliamo il prezzo in base ai kilometri (0.21 € per km).
+4. Applichiamo il 20 % per i < di 18 anni.
+5. Applichiamo il 40% per i > di 65 anni.
+6. Stampiamo in pagina.
+Bonus Mostriamo in pagina.
+*/
+
+// Recupero l'elemento in pagina-Bonus
+
+
+// 1
+
+// 2
+
+
+// 3
+
+
+// 4
+
+
+//Bonus 
